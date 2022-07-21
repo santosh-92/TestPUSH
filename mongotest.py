d0932a8fb7506a62be23e8572b8d5f9893fb389c
Santosh <santuhappy123@gmail.com>
2022-07-21 21:07:13 +0530

    This is my first modified code push in git

--- mongotest.py
+++ mongotest.py
@@ -10,6 +10,26 @@ d =  {
 
 }
 
+d =  {
+    "name" : "santosh",
+    "email" : "santuhappy123@gmail.com",
+    "surname" : "sattigeri"
+
+}
+
+d =  {
+    "name" : "santosh",
+    "email" : "santuhappy123@gmail.com",
+    "surname" : "sattigeri"
+
+}
+d =  {
+    "name" : "santosh",
+    "email" : "santuhappy123@gmail.com",
+    "surname" : "sattigeri"
+
+}
+
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
